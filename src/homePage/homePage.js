3ffa4caaae1939de79620b6a8ec54b9a057549ae
Aamsegal <Amsegal93@gmail.com>
2024-07-11 19:48:02 -0400

    Added core React routing capabilities

---
+++ src/homePage/homePage.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+import "./homePage.css";
+
+function HomePage(props) {
+
+    return(
+        <div className="homePageContainer">
+
+            <h1>HomePage</h1>
+
+        </div>
+    )
+
+};
+
+export default HomePage;
---
+++ src/noPageFound/noPageFound.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+import "./noPageFound.css";
+
+function NoPageFound(props) {
+
+    return (
+        <div className="noPageFoundContainer">
+
+            <h1>Stuff Broken</h1>
+
+        </div>
+    )
+
+};
+
+export default NoPageFound;
